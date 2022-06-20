e88dcbdf9ecb1db6fa055635cafac9b23ca0405b
jspark1152 <jspark1152@gmail.com>
2022-06-20 14:51:54 +0900

    0620 test

--- 21_2_1.r
+++ 21_2_1.r
@@ -19,3 +19,41 @@ data_80 <- BostonHousing %>% filter(BostonHousing$age >= 80)
 head(data_80, 20)
 
 mean(data_80$crim)
+
+#2번
+ds_init <- read.csv('housing.csv')
+#첫행부터 80%행까지 추출
+ds <- ds_init[1:(nrow(ds_init)*0.8),]
+#결측값 확인
+summary(ds$total_bedrooms)
+#중위값 확인
+med_totb <- median(ds$total_bedrooms, na.rm=TRUE)
+med_totb
+#전처리 전의 표준편차 확인
+sd_before <- sd(ds$total_bedrooms, na.rm=TRUE)
+sd_before
+#전처리
+ds$total_bedrooms <- ifelse(is.na(ds$total_bedrooms)==1, med_totb, ds$total_bedrooms)
+summary(ds$total_bedrooms)
+#전처리 후의 표준편차 확인
+sd_after <- sd(ds$total_bedrooms)
+sd_after
+#결과 정리
+result <- abs(sd_before - sd_after)
+print(result)
+
+#3번
+#데이터셋 확인
+ds_3 <- read.csv('insurance.csv')
+summary(ds_3$charges)
+#이상값 기준 설정
+mean_ch <- mean(ds_3$charges)
+sd_ch <- sd(ds_3$charges)
+sup_ch <- mean_ch + 1.5*sd_ch
+inf_ch <- mean_ch - 1.5*sd_ch
+#설정한 이상값 기준을 토대로 필터링
+#out_ds_3_ch <- ds_3 %>% filter(ds_3$charges >= sup_ch | ds_3$charges <= inf_ch)
+result_3 <- ds_3 %>% filter(charges >= sup_ch | charges <= inf_ch) %>% summarise(sum(charges))
+#결과 도출
+#result_3 <- sum(out_ds_3_ch$charges)
+print(result_3)
---
+++ 21_3_1.r
@@ -0,0 +1,45 @@
+#1번
+ds_1 <- read.csv('housing.csv')
+summary(ds_1)
+#결측값 제거
+ds_1 <- na.omit(ds_1)
+summary(ds_1)
+#훈련 데이터 세팅
+train_ds_1 <- ds_1[1:(nrow(ds_1)*0.7),]
+#결과 도출
+summary(train_ds_1$housing_median_age)
+result_1 <- as.integer(summary(train_ds_1$housing_median_age)[2])
+print(result_1)
+
+#2번 이건 어렵네 >> 문제에 낚임
+#데이터 확인
+ds_2 <- read.csv('train.csv')
+str(ds_2)
+summary(ds_2)
+#결측값 개수 확인
+colSums(is.na(ds_2))
+#결측값의 비율 확인
+na_ds_2 <- colSums(is.na(ds_2))/nrow(ds_2)
+data.frame(na_ds_2)
+#결과 도출
+sort_na <- sort(na_ds_2, decreasing=TRUE)
+sort_na
+#변수의 이름을 출력해야함
+print(names(sort_na)[1])
+
+#3번
+ds_3 <- read.csv('TB_notifications.csv')
+ds_3 <- ds_3[, c('country', 'year', 'new_sp')]
+ds_3 <- ds_3 %>% filter(ds_3$year == 2000)
+ds_3 <- na.omit(ds_3)
+str(ds_3)
+
+#평균
+summary(ds_3)
+mean_sp <- mean(ds_3$new_sp)
+mean_sp
+
+#필터링
+ds_3 <- ds_3 %>% filter(new_sp > mean_sp)
+result <- nrow(ds_3)
+print(result)
